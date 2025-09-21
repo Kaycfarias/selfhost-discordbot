717ced50261ad9d094080f7afbe4500663c05537
Kaycfarias <kaycg123@gmail.com>
2025-09-21 09:01:38 -0300

    feat: add terminal component with WebSocket intergration for bot interaction

--- src/app/apps/[botId]/page.tsx
+++ src/app/apps/[botId]/page.tsx
@@ -1,10 +1,20 @@
-interface BotPageProps {
-  params: Promise<{ botId: string }>;
-}
+"use client";
 
-const BotPage = async ({ params }: BotPageProps) => {
-  const { botId } = await params;
-  return <>{botId}</>;
+import { useParams } from "next/navigation";
+import dynamic from "next/dynamic";
+const TerminalOutput = dynamic(() => import("@/components/terminal"), {
+  ssr: false,
+});
+
+const BotPage = () => {
+  const { botId } = useParams();
+
+  return (
+    <div className="size-full p-4">
+      <h1>Bot ID: {botId}</h1>
+      <TerminalOutput botId={botId as string} />
+    </div>
+  );
 };
 
 export default BotPage;
